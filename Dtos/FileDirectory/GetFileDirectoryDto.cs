af0299873112c31980ea62598efcbceeab32103c
southpacificinc <jdc3152@southpacificinc.com>
2020-12-07 14:17:36 +0800

    Update FileDirectoryDto

--- Dtos/FileDirectory/GetFileDirectoryDto.cs
+++ Dtos/FileDirectory/GetFileDirectoryDto.cs
@@ -6,13 +6,10 @@ namespace dms_api.Dtos.FileDirectory
     public class GetFileDirectoryDto
     {
         public int Id { get; set; }
-        public int? RootDirectoryId { get; set; }
         public GetRootDirectoryDto RootDirectory { get; set; }
-        public int CatalogId { get; set; }
         public GetCatalogDto Catalog { get; set; }
         public string Name { get; set; }
         public string Path { get; set; }
-        public int? ParentId { get; set; }
         public GetFileDirectoryDto Parent { get; set; }
     }
 }
